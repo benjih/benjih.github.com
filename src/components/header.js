b36f2345ce7b8eb7b2bc2d69d5617c60675d983b
Benji Hooper <benji@sixp.in>
2019-05-15 00:59:13 +0100

    Complete teardown

--- src/components/header.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class Header extends Component {
-  render() {
-    return (
-        <h1 style={{}}>{this.props.children}</h1>
-    )
-  }
-}
